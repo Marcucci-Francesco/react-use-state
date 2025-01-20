1fe226c1b4019d1b65aa77491f7db24567308843
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-01-20 17:34:31 +0100

    feat: add completed main functions

--- Components/main.jsx
+++ Components/main.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
+
 const main = (props) => {
 
   const languages = props.languages;
+  const [selectId, setSelectId] = useState(null);
+
+  const handlerToggle = (id) => {
+    setSelectId(id)
+  }
 
   return (
     <main>
       <div className='container'>
         <div className='d-flex'>
           {languages.map(item => (
-            <button key={item.id}>{item.title}</button>
+            <button key={item.id} onClick={() => handlerToggle(item.id)}>{item.title}</button>
           ))}
         </div>
         {languages.map(item => (
-          <div className="card" key={item.id}>
-            <div className='card-text'>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
+          selectId == item.id && (
+            <div className="card" key={item.id}>
+              <div className='card-text'>
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+              </div>
             </div>
-          </div>
+          )
         ))}
       </div>
     </main>
